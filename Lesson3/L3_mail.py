8cbcc7edafaa011b26102e474ae92c6fe6c0dbfd
Marg0 <Marg0@MacBook-Pro.local>
2024-03-20 00:39:01 +0300

    Hw3

---
+++ Lesson3/L3_mail.py
@@ -0,0 +1,9 @@
+from L3_address import Address
+class Mailing:
+    def __init__(self, to_address, from_address, track, cost):
+        self.address = to_address
+        self.f_address = from_address
+        self.track = track
+        self.cost = cost
+
+
---
+++ Lesson3/L3_smartphone.py
@@ -0,0 +1,7 @@
+class Smartphone:
+    def __init__(self, brend, model, subscriber_num):
+        print("создано")
+        self.brend= brend
+        self.model = model
+        self.number= subscriber_num
+        
