3b6e593c5d90d3d08a99fcb15a46f6463b65678b
M@rG0 <139683680+Marg0qa@users.noreply.github.com>
2024-03-21 02:43:26 -0700

    Delete Lesson3 directory

--- Lesson3/L3_mail.py
+++
@@ -1,9 +0,0 @@
-from L3_address import Address
-class Mailing:
-    def __init__(self, to_address, from_address, track, cost):
-        self.address = to_address
-        self.f_address = from_address
-        self.track = track
-        self.cost = cost
-
-
--- Lesson3/L3_task3.py
+++
@@ -1,8 +0,0 @@
-from L3_address import Address
-from L3_mail import Mailing
-
-to_address = Address("682920", "пгт.Хор", "ул.Ленина", "12", "23")
-from_address = Address("196244", "Санкт-Петербург", "проспект Космонавтов", "50", "4")
-mailing = Mailing(to_address, from_address, "RU682277243567", 1500)
-
-print(f"Отправление {mailing.track} из {to_address.index}, {to_address.city}, {to_address.street}, {to_address.house} - {to_address.apartment} в {from_address.index}, {from_address.city}, {from_address.street}, {from_address.house} - {from_address.apartment}. Стоимость {mailing.cost} рублей.")
